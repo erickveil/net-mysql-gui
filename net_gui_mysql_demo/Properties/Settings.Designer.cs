864b78fe22ab3df2169adabe3c6912eb8e240efc
Erick Veil <erick@erickveil.net>
2014-02-01 15:59:38 -0800

    added database connection in visual studio

--- net_gui_mysql_demo/Properties/Settings.Designer.cs
+++ net_gui_mysql_demo/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace net_gui_mysql_demo.Properties
-{
-
-
+namespace net_gui_mysql_demo.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "9.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=localhost;User Id=root;Persist Security Info=True;database=gui_test")]
+        public string gui_testConnectionString {
+            get {
+                return ((string)(this["gui_testConnectionString"]));
             }
         }
     }
