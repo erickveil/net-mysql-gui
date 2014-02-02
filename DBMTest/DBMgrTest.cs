d56250e3d80be4251cd6c46fd772e2f00ce134e4
Erick Veil <erick@erickveil.net>
2014-02-01 16:55:45 -0800

    added unit tests

---
+++ DBMTest/DBMgrTest.cs
@@ -0,0 +1,165 @@
+﻿using net_gui_mysql_demo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace DBMTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DBMgrTest and is intended
+    ///to contain all DBMgrTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DBMgrTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for searchForValue
+        ///</summary>
+        [TestMethod()]
+        public void searchForValueTest()
+        {
+            DBMgr target = new DBMgr(); // TODO: Initialize to an appropriate value
+            string Name = string.Empty; // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.searchForValue(Name);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Finalize
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("net_gui_mysql_demo.exe")]
+        public void FinalizeTest()
+        {
+            DBMgr_Accessor target = new DBMgr_Accessor(); // TODO: Initialize to an appropriate value
+            target.Finalize();
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for editEntry
+        ///</summary>
+        [TestMethod()]
+        public void editEntryTest()
+        {
+            DBMgr target = new DBMgr(); // TODO: Initialize to an appropriate value
+            int key = 0; // TODO: Initialize to an appropriate value
+            string name = string.Empty; // TODO: Initialize to an appropriate value
+            string value = string.Empty; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.editEntry(key, name, value);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for deleteEntry
+        ///</summary>
+        [TestMethod()]
+        public void deleteEntryTest()
+        {
+            DBMgr target = new DBMgr(); // TODO: Initialize to an appropriate value
+            string name = string.Empty; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.deleteEntry(name);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for databaseConnect
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("net_gui_mysql_demo.exe")]
+        public void databaseConnectTest()
+        {
+            DBMgr_Accessor target = new DBMgr_Accessor(); // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.databaseConnect();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for addEntry
+        ///</summary>
+        [TestMethod()]
+        public void addEntryTest()
+        {
+            DBMgr target = new DBMgr(); // TODO: Initialize to an appropriate value
+            string name = string.Empty; // TODO: Initialize to an appropriate value
+            string value = string.Empty; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.addEntry(name, value);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for DBMgr Constructor
+        ///</summary>
+        [TestMethod()]
+        public void DBMgrConstructorTest()
+        {
+            DBMgr target = new DBMgr();
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+    }
+}
--- DBMTest/Properties/AssemblyInfo.cs
+++ DBMTest/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("net_gui_mysql_demo")]
+[assembly: AssemblyTitle("DBMTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net_gui_mysql_demo")]
+[assembly: AssemblyProduct("DBMTest")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5693de6a-7b70-4388-839f-293c07b3cb5b")]
+[assembly: Guid("5eb700bd-9c39-4bd6-aa4b-b00228062ab2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DBMTest")]
