7df43d643f5ab58dfb10cdbe3950df4787eaa8f3
Erick Veil <erick@erickveil.net>
2014-02-01 16:12:40 -0800

    added database manager classs

---
+++ net_gui_mysql_demo/DBMgr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace net_gui_mysql_demo
+{
+    class DBMgr
+    {
+        public DBMgr()
+        {
+        }
+
+        ~DBMgr()
+        {
+        }
+
+        private bool databaseConnect()
+        {
+            return false;
+        }
+
+        public string searchForValue(string Name)
+        {
+            return "";
+        }
+
+        public bool addEntry(string name, string value)
+        {
+            return false;
+        }
+
+        public bool editEntry(int key, string name, string value)
+        {
+            return false;
+        }
+
+        public bool deleteEntry(string name)
+        {
+            return false;
+        }
+    }
+}
