e164950841107dcb3babf086d75ef685ffae1d52
Erick Veil <erick@erickveil.net>
2014-02-01 20:49:25 -0800

    database connection test passed

--- net_gui_mysql_demo/DBMgr.cs
+++ net_gui_mysql_demo/DBMgr.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
 namespace net_gui_mysql_demo
 {
     class DBMgr
     {
-        public DBMgr(string user, string password)
+        private string server="localhost";
+        private string database="gui_test";
+        private string user = "";
+        private string pw = "";
+        private MySqlConnection connection = null;
+
+        public DBMgr(string p_user, string p_password)
         {
+            user = p_user;
+            pw = p_password;
         }
 
         ~DBMgr()
         {
+            pw = "";
         }
 
         private bool databaseConnect()
         {
-            return false;
+            string con_str = "SERVER=" + server + ";" +
+                "DATABASE=" + database + ";" +
+                "UID=" + user + ";" +
+                "PASSWORD=" + pw + ";";
+
+            try
+            {
+                connection = new MySqlConnection(con_str);
+                connection.Open();
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public string searchForValue(string Name)
